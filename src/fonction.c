80cd37cb81871df5d75a02374181b819914b5720
LeReverandNox_OSX <lereverandnox@gmail.com>
2014-10-02 17:39:59 +0200

    Déplacement du code de la devinette dans une nouvelle fonction et création du fichier fonction.c + son header

---
+++ src/fonction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int question(void)
+{
+	int reponse = 0;
+
+	printf("Quel est la réponse à la grande question de la vie, de l'univers et tout le reste ? ");
+	scanf("%d", &reponse);
+
+	if (reponse == 42)
+	{
+		printf("Vous etes un être humain valeureux !\nJe suis fier de vous !\n");
+	}
+	else
+	{
+		printf("Hors de ma vue, ignorant !\n");
+	}
+	return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fonction.h"
 
 int main (void)
 {
-	int reponse = 0;
-
-	printf("Quel est la réponse à la grande question de la vie, de l'univers et tout le reste ? ");
-	scanf("%d", &reponse);
-
-	if (reponse == 42)
-	{
-		printf("Vous etes un être humain valeureux !\nJe suis fier de vous !\n");
-	}
-	else
-	{
-		printf("Hors de ma vue, ignorant !\n");
-	}
+	question();
 
 	return 0;
 }
