cd98122fc1032af5bb64bff470e1607bc90f7823
LeReverandNox_OSX <lereverandnox@gmail.com>
2014-10-02 17:49:06 +0200

    Rajout d'un commentaire d'intro dans le main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+/*
+ * Un petit programme inutile, histoire de meubler un peu le repo
+ * Sinon, ca va chez vous ?
+ * Belle journée, bien mangé, toussa toussa ?
+ * #LaBise
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "fonction.h"
