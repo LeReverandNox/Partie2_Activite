8895e26bf789694d4d3983a45c8b0daff4be50a9
LeReverandNox_OSX <lereverandnox@gmail.com>
2014-10-02 17:31:50 +0200

    Remplacement du Hello World par une petite devinette

--- src/main.c
+++ src/main.c
@@ -3,7 +3,20 @@
 
 int main (void)
 {
-	printf("Hello World !\n");
+	int reponse = 0;
+
+	printf("Quel est la réponse à la grande question de la vie, de l'univers et tout le reste ? ");
+	scanf("%d", &reponse);
+
+	if (reponse == 42)
+	{
+		printf("Vous etes un être humain valeureux !\nJe suis fier de vous !\n");
+	}
+	else
+	{
+		printf("Hors de ma vue, ignorant !\n");
+	}
+
 	return 0;
 }
 
